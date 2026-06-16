9a990354e83e49add78c2bcc325f59ef67df7ac4
Luisa Folle <a26110039@ceti.mx>
2026-06-16 17:17:49 -0600

    Folle agrega interfaz de usuario

--- calculadora.c
+++ calculadora.c
@@ -30,8 +30,20 @@ scanf("%f",&num1);
 printf("Ingrese segundo numero: ");
 scanf("%f",&num2);
 
-
+printf("\nGracias por usar la calculadora\n");
 
 return 0;
 
+printf("=========================\n");
+printf("   CALCULADORA DIGITAL\n");
+printf("=========================\n");
+
+
+printf("Seleccione una operacion:\n");
+
+printf("1. Suma\n");
+printf("2. Resta\n");
+printf("3. Multiplicacion\n");
+printf("4. Division\n");
 }
+
